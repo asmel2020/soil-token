bd2143c7f9c1bdf90f06ab7814b34100ef69820f
Danny Gonzalez <danny.galicia@rhizom.me>
2023-08-24 15:17:52 -0300

    feat: remove contract

--- contracts/HelloWorld.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-contract HelloWorld  {
-
-    function helloWorld() public pure returns (string memory) {
-        return "Hello World";
-    }
-}
